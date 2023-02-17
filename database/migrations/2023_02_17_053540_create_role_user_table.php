cae394c0bfc5b3e9f4bdf56023a389db7b84b261
shadreza <shadreza100@gmail.com>
2023-02-17 11:50:15 +0600

    tables migrated

--- database/migrations/2023_02_17_053540_create_role_user_table.php
+++ database/migrations/2023_02_17_053540_create_role_user_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->integer('role_id')->unsigned()->nullable()->index();
+            $table->integer('user_id')->unsigned()->nullable()->index();
             $table->timestamps();
         });
     }
